0aaab1209e462fb2b8dd417d2acef37862eac71e
Thabish <kadertabish2@gmail.com>
2023-01-05 16:15:57 +0400

    cors-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,13 @@ import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import { config } from "dotenv";
 import Deck from "./models/Deck";
-
+import cors from "cors";
 // CONFIGURATION
 config();
 const app = express();
 const PORT = 5001;
 app.use(express.json());
+app.use(cors());
 
 app.get("/decks", async (req: Request, res: Response) => {
 	const Decks = await Deck.find();
