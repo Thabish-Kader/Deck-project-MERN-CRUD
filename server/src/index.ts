556fc126d3598aaea9b5d31e80c258cc05ddadcf
Thabish <kadertabish2@gmail.com>
2023-01-05 16:11:10 +0400

    get-done
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,28 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import { config } from "dotenv";
+import Deck from "./models/Deck";
 
+// CONFIGURATION
 config();
 const app = express();
 const PORT = 5001;
+app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-	res.send("hello world");
+app.get("/decks", async (req: Request, res: Response) => {
+	const Decks = await Deck.find();
+	res.json(Decks);
 });
 
+app.post("/decks", async (req: Request, res: Response) => {
+	const newDeck = new Deck({
+		title: req.body.title,
+	});
+	const createdDeck = await newDeck.save();
+	res.status(200).json({ createdDeck });
+});
+
+// Connection
 mongoose.connect(process.env.MONGO_URL!).then(() => {
 	console.log(`listening on port ${PORT}`);
 	app.listen(PORT);
