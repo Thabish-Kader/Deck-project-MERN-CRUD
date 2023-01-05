306d2f40a5beba7b62e6668ea13e259e094429e5
Thabish <kadertabish2@gmail.com>
2023-01-05 15:29:47 +0400

    connection-made
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
 
+config();
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("hello world");
 });
 
-app.listen(5001);
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+	console.log(`listening on port ${PORT}`);
+	app.listen(PORT);
+});
